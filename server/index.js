e8268d069621426f1512b94459ba6963e6a96e6e
dheerajpatel56 <dheerajpatel2275@gmail.com>
2026-02-25 15:57:05 +0530

    fix: update catch-all route for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/api/admin', adminRoutes);
 app.use(express.static(path.join(__dirname, '../client')));
 
 // Catch-all to serve the frontend for any non-API routes
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/login.html'));
 });
 
